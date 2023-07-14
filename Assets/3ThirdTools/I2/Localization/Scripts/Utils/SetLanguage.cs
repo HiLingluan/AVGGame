7124a8094490c27e7500432e33601ec2916ad926
LingLuan <656897516@qq.com>
2023-07-14 11:45:11 +0800

    添加多语言插件

---
+++ Assets/3ThirdTools/I2/Localization/Scripts/Utils/SetLanguage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+namespace I2.Loc
+{
+	[AddComponentMenu("I2/Localization/SetLanguage Button")]
+	public class SetLanguage : MonoBehaviour 
+	{
+		public string _Language;
+
+#if UNITY_EDITOR
+		public LanguageSource mSource;
+#endif
+		
+
+		public void ApplyLanguage()
+		{
+			if( LocalizationManager.HasLanguage(_Language))
+			{
+				LocalizationManager.CurrentLanguage = _Language;
+			}
+            else
+            {
+				//LoggerHelper.CommLogError("不存在此语言=================="+ _Language);
+            }
+		}
+    }
+}
