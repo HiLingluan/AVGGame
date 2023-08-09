feaaa168d2c14ba42566c115aecdcdc10a294681
LingLuan <656897516@qq.com>
2023-08-09 14:55:37 +0800

    增加阅读场景启动流程

---
+++ Assets/Scripts/CS/Procedure/ProcedureRead.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using System.Collections.Generic;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace Game
+{
+    public class ProcedureRead : ProcedureBase
+    {
+        public override bool UseNativeDialog
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        protected override void OnInit(ProcedureOwner procedureOwner)
+        {
+            base.OnInit(procedureOwner);
+
+            
+        }
+
+        protected override void OnDestroy(ProcedureOwner procedureOwner)
+        {
+            base.OnDestroy(procedureOwner);
+
+        }
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+            GameEntry.Scene.LoadScene(AssetUtility.GetSceneAsset("Read"), Constant.AssetPriority.SceneAsset, this);
+        }
+
+    }
+}
